9e7d5c93e6545686a524c2832358d2c1a3f51a92
Tabor <86973131+TaborMather-dev@users.noreply.github.com>
2021-08-24 07:28:39 -0700

    made several component changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import VideoPlayer from './VideoPlayer';
+import VideoPlayer from './components/VideoPlayer';
 
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-        this.Comments = [
-
-        ];
-    }
+    return
     render() {
         return (
             <div className="container-fluid">
---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+class Comment extends Component() {
+    constructor(props) {
+        super(props)
+        this.state = 
+}
+
+    export default AddComment;
